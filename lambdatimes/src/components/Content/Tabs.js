42d84061b7ff1336f7217f18878814dc0938c19a
Maxime Salomon <maxime@croissant.io>
2019-02-22 09:34:17 +0100

    implemented trending topics

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import Tab from './Tab';
+
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TabStyle = styled.p `
+  margin-right: 10px;
+  margin-left: 10px;
+`
+
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {
+              props.tabs.map((tab) => (
+                <TabStyle key={tab}>{tab}</TabStyle>
+              ))
+            }
+
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.string
+  ).isRequired
+
+};
+
 export default Tabs;
