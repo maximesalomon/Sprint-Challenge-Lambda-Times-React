1995f1707c6b576f02caffff9575d87091807315
Maxime Salomon <maxime@croissant.io>
2019-02-22 11:11:19 +0100

    filtered cards feature

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -21,24 +21,16 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     this.setState({selected: tab})
-
   };
 
 
   filterCards = () => {
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
-
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
+   if(this.state.selected === 'all') {
     return this.state.cards;
+   } else {
+    let filteredCards = this.state.cards.filter((card) => card.tab.includes(this.state.selected));
+     return filteredCards;
+   }
   };
 
   render() {
