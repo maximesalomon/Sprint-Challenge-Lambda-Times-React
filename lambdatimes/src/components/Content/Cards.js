0cd0661cf022116ad5920189dce2431146086b59
Maxime Salomon <maxime@croissant.io>
2019-02-22 10:23:02 +0100

    added PropTypes checks

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {
         props.cards.map((card) =>(
-          <Card card={card}/>
+          <Card key={card.headline} card={card}/>
         ))
       }
     </div>
   )
 }
 
-// Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string,
+      tab: PropTypes.string,
+      img: PropTypes.string,
+      author: PropTypes.string
+    })
+  ).isRequired
+};
 
 export default Cards;
